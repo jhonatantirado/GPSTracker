64ecf079cca87b190cce8a33e647c16bd3d415c4
jhonatantirado <jhonatantiradotiradodeep@gmail.com>
2017-03-04 01:23:52 -0500

    Validate sender

--- app/src/main/java/org/traccar/client/SMSReceiver.java
+++ app/src/main/java/org/traccar/client/SMSReceiver.java
@@ -32,22 +32,10 @@ public class SMSReceiver extends BroadcastReceiver {
     private AlarmManager alarmManager;
     private PendingIntent alarmIntent;
 
-    public static final String KEY_DEVICE = "id";
-    public static final String KEY_ADDRESS = "address";
-    public static final String KEY_PORT = "port";
-    public static final String KEY_SECURE = "secure";
-    public static final String KEY_INTERVAL = "interval";
-    public static final String KEY_PROVIDER = "provider";
-    public static final String KEY_STATUS = "status";
-    public static final String KEY_CELLPHONE = "cellphone";
-    public static final String KEY_SENDTOSERVER = "SendToServer";
-    public static final String KEY_SENDSMS ="SendSMS";
-
     @Override
     public void onReceive(Context context, Intent intent) {
 
         preferences = PreferenceManager.getDefaultSharedPreferences(context);
-
         cellphone = preferences.getString(MainActivity.KEY_CELLPHONE, Constants.CELLPHONE);
 
         SmsMessage[] messages = null;
@@ -67,36 +55,47 @@ public class SMSReceiver extends BroadcastReceiver {
 
                 messages = new SmsMessage[pdus.length];
 
+                outerloop:
                 for (int i = 0; i < messages.length; i++) {
+
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         String format = myBundle.getString("format");
                         messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
                     } else {
                         messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                     }
-                    strMessage += "SMS From: " + messages[i].getOriginatingAddress();
+
+                    String sender="";
+                    //sender = messages[i].getOriginatingAddress();
+                    sender = messages[i].getDisplayOriginatingAddress();
+                    String messageBody = "";
+                    messageBody = messages[i].getMessageBody();
+
+                    strMessage += "SMS From: " + sender;
                     strMessage += " : ";
-                    strMessage += messages[i].getMessageBody();
+                    strMessage += messageBody;
                     strMessage += "\n";
+
+                    if (sender.toUpperCase().contains(cellphone.toUpperCase()))
+                    {
+                        if (messageBody.toUpperCase().contains(Constants.START_MESSAGE.toUpperCase())) {
+                            Log.d("REMOTE", "Starting service");
+                            context.startService(new Intent(context, TrackingService.class));
+                            break outerloop;
+                        }
+
+                        if (messageBody.toUpperCase().contains(Constants.STOP_MESSAGE.toUpperCase())) {
+                            Log.d("REMOTE", "Stopping service");
+                            context.stopService(new Intent(context, TrackingService.class));
+                            break outerloop;
+                        }
+
+                    }
+
                 }
                 Log.d("SMS content", strMessage);
-
-                if (strMessage.toString().contains(Constants.START_MESSAGE)) {
-                    Log.d("REMOTE", "Starting service");
-                    context.startService(new Intent(context, TrackingService.class));
-                }
-
-                if (strMessage.toString().contains(Constants.STOP_MESSAGE)) {
-                    Log.d("REMOTE", "Stopping service");
-                    stopTrackingService(context);
-                }
-
             }
         }
-    }
-
-    private void stopTrackingService(Context context) {
-        context.stopService(new Intent(context, TrackingService.class));
     }
 
 }
