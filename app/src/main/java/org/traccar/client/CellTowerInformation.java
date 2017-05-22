09fcb02e97475e5a30e7d15bd42faaff946e608e
jhonatantirado <jhonatantiradotiradodeep@gmail.com>
2017-05-21 19:43:35 -0500

    Add Mozilla Location Services constants

---
+++ app/src/main/java/org/traccar/client/CellTowerInformation.java
@@ -0,0 +1,8 @@
+package org.traccar.client;
+
+/**
+ * Created by Nathan on 21/05/2017.
+ */
+
+public class CellTowerInformation {
+}
--- app/src/main/java/org/traccar/client/Constants.java
+++ app/src/main/java/org/traccar/client/Constants.java
@@ -12,4 +12,7 @@ public class Constants {
     public static String STOP_MESSAGE = "morita";
     public static String CELLTOWER_POSITION = "lionbebe";
     public static String LAUNCHER_NUMBER = "*#232#*";
+    public static String MOZILLA_LOCATION_SERVICES_URL = "https://location.services.mozilla.com/";
+    public static String API_VERSION = "v1/geolocate";
+    public static String API_KEY = "test";
 }
