110b24153c0bcfd653d5656e1c8fb53a73c1d4e0
jhonatantirado <jhonatantiradotiradodeep@gmail.com>
2017-05-21 19:44:05 -0500

    Add CellTowerInformation class

--- app/src/main/java/org/traccar/client/CellTowerInformation.java
+++ app/src/main/java/org/traccar/client/CellTowerInformation.java
@@ -5,4 +5,39 @@ package org.traccar.client;
  */
 
 public class CellTowerInformation {
+
+    private String mobileCountryCode;
+    public String getMobileCountryCode(){return mobileCountryCode;}
+    public void setMobileCountryCode(String mobileCountryCode){this.mobileCountryCode=mobileCountryCode;}
+
+    private String mobileNetworkCode;
+    public String getMobileNetworkCode(){return mobileNetworkCode;}
+    public void setMobileNetworkCode(String mobileNetworkCode){this.mobileNetworkCode=mobileNetworkCode;}
+
+    private String locationAreaCode;
+    public String getLocationAreaCode(){return locationAreaCode;}
+    public void setLocationAreaCode(String locationAreaCode){this.locationAreaCode=locationAreaCode;}
+
+    private String cellId;
+    public String getCellId(){return cellId;}
+    public void setCellId (String cellId){this.cellId=cellId;}
+
+    private String radioType;
+    public String getRadioType() {return radioType;}
+
+    public void setRadioType(String radioType) {
+        this.radioType = radioType;
+    }
+
+    public CellTowerInformation(){
+    }
+
+    public CellTowerInformation(String mobileCountryCode, String mobileNetworkCode, String locationAreaCode, String cellId, String radioType){
+        this.mobileCountryCode = mobileCountryCode;
+        this.mobileNetworkCode = mobileNetworkCode;
+        this.locationAreaCode = locationAreaCode;
+        this.cellId = cellId;
+        this.radioType = radioType;
+    }
+
 }
